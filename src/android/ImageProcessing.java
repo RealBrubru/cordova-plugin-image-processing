9ac13ec986eb731f9c4ded4f12613481c816e5dc
Bruno Oliveira <falecomigo@brubru.com.br>
2016-02-16 23:11:50 -0200

    Adding log

--- src/android/ImageProcessing.java
+++ src/android/ImageProcessing.java
@@ -6,10 +6,17 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.util.Log;
+
 public class ImageProcessing extends CordovaPlugin {
+    
+    public static final String LOG_TAG = "ImageProcessing";
     
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        Log.d(LOG_TAG, "action: " + action);
+        Log.d(LOG_TAG, "args: " + args.toString());
+        
         callbackContext.success();
         return true;
     }
