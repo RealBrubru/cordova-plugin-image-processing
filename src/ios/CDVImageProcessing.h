d19233d65281c06dc561a27dd77d67cfc669915f
Bruno Oliveira <falecomigo@brubru.com.br>
2016-02-16 23:46:16 -0200

    implementing IOS methods

---
+++ src/ios/CDVImageProcessing.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface CDVImageProcessing : CDVPlugin {
+}
+
+- (void) resize:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ src/ios/CDVImageProcessing.m
@@ -0,0 +1,11 @@
+#import "CDVImageProcessing.h"
+#import <Cordova/CDV.h>
+
+@implementation CDVImageProcessing
+
+- (void) resize:(CDVInvokedUrlCommand*)command
+{
+    NSLog(@"CDVImageProcessing IOS resize");
+}
+
+@end
