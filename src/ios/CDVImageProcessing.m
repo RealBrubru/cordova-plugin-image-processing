c46871e8967de86be66b018f08acafad803a159e
Bruno Oliveira <falecomigo@brubru.com.br>
2016-02-17 23:50:50 -0200

    fix

--- src/ios/CDVImageProcessing.m
+++ src/ios/CDVImageProcessing.m
@@ -8,8 +8,8 @@
     NSString *sourceUri = [command argumentAtIndex: 0];
     NSString *destinationUri = [command argumentAtIndex: 1];
     NSNumber *width = [command argumentAtIndex: 2];
-    //NSNumber *height = [command argumentAtIndex: 3];
-    //BOOL *keepScale = [command argumentAtIndex: 4];
+    NSNumber *height = [command argumentAtIndex: 3];
+    BOOL keepScale = [command argumentAtIndex: 4];
     
     NSLog(@"CDVImageProcessing AAAAAAAHHH");
 }
