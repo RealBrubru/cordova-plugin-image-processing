209ce1fb798b4244cddb2b1036151961ca513653
Bruno Oliveira <falecomigo@brubru.com.br>
2016-02-17 23:45:36 -0200

    fix

--- src/ios/CDVImageProcessing.h
+++ src/ios/CDVImageProcessing.h
@@ -1,6 +1,5 @@
 #import <Foundation/Foundation.h>
 #import <Cordova/CDVPlugin.h>
-#import <Cordova/CDVInvokedUrlCommand.h>
 
 @interface CDVImageProcessing : CDVPlugin {}
 
--- src/ios/CDVImageProcessing.m
+++ src/ios/CDVImageProcessing.m
@@ -1,15 +1,15 @@
-#import "CDVImageProcessing.h"
 #import <Cordova/CDV.h>
+#import "CDVImageProcessing.h"
 
 @implementation CDVImageProcessing
 
 - (void) resize:(CDVInvokedUrlCommand *)command
 {
-    NSString *sourceUri = [command argumentAtIndex: 0];
-    NSString *destinationUri = [command argumentAtIndex: 1];
-    NSNumber *width = [command argumentAtIndex: 2];
-    NSNumber *height = [command argumentAtIndex: 3];
-    BOOL *keepScale = [command argumentAtIndex: 4];
+    //NSString *sourceUri = [command argumentAtIndex: 0];
+    //NSString *destinationUri = [command argumentAtIndex: 1];
+    //NSNumber *width = [command argumentAtIndex: 2];
+    //NSNumber *height = [command argumentAtIndex: 3];
+    //BOOL *keepScale = [command argumentAtIndex: 4];
     
     NSLog(@"CDVImageProcessing AAAAAAAHHH");
 }
