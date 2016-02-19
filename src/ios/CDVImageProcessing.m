cae4d12392030a80885344c028dce2000a510a0b
Bruno Oliveira <falecomigo@brubru.com.br>
2016-02-19 14:28:39 -0200

    adding code to resize

--- src/ios/CDVImageProcessing.m
+++ src/ios/CDVImageProcessing.m
@@ -5,17 +5,25 @@
 
 - (void) resize:(CDVInvokedUrlCommand *)command
 {
+    NSLog(@"CDVImageProcessing - resize - start");
+        
     NSString *sourceUri = [command argumentAtIndex: 0];
     NSString *destinationUri = [command argumentAtIndex: 1];
     NSNumber *width = [command argumentAtIndex: 2];
     NSNumber *height = [command argumentAtIndex: 3];
     BOOL keepScale = [command argumentAtIndex: 4];
     
-    
-    
-    NSLog(@"CDVImageProcessing AAAAAAAHHH");
-}
+    UIImage *originalImage = self loadImage:sourceUri];
 
+    UIImage *resizedImage = [self resizeImage:originalImage toSize:CGSizeMake(width, height) andKeepScale:keepScale];
+
+    [self saveImage:resizedImage toFilePath:destinationUri];
+
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image resized"];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+
+    NSLog(@"CDVImageProcessing - resize - stop");
+}
 
 - (void) rotate:(CDVInvokedUrlCommand *)command
 {
@@ -25,6 +33,67 @@
 - (void) crop:(CDVInvokedUrlCommand *)command
 {
     NSLog(@"CDVImageProcessing AAAAAAAHHH");
+}
+
+
+
+
+- (UIImage *)resizeImage:(UIImage *)originalImage toSize:(CGSize)size andKeepScale:(BOOL)keepScale {
+    CGImageRef cgOriginalImage = originalImage.CGImage;
+    
+    if (keepScale) {
+        size = [self estimatedScaleSize:size forImage:originalImage];
+    }
+
+    size_t bitsPerComponent = CGImageGetBitsPerComponent(cgOriginalImage);
+    size_t bytesPerRow = CGImageGetBytesPerRow(cgOriginalImage);
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(cgOriginalImage);
+    CGBitmapInfo info = CGImageGetBitmapInfo(cgOriginalImage);
+    
+    CGContextRef context = CGBitmapContextCreate(nil, size.width, size.height, bitsPerComponent, bytesPerRow, colorSpace, info);
+    
+    CGContextSetInterpolationQuality(context, kCGInterpolationMedium);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, size.width, size.height), cgOriginalImage);
+    
+    CGImageRef cgResizedImage = CGBitmapContextCreateImage(context);
+    
+    UIImage *resizedImage = [UIImage imageWithCGImage:cgResizedImage];
+    
+    CGContextRelease(context);
+    CGImageRelease(cgOriginalImage);
+    CGImageRelease(cgResizedImage);
+
+    return resizedImage;
+}
+
+- (CGSize)estimatedScaleSize:(CGSize)newSize forImage:(UIImage *)image {
+    if (image.size.width > image.size.height) {
+        newSize = CGSizeMake((image.size.width / image.size.height) * newSize.height, newSize.height);
+    } else {
+        newSize = CGSizeMake(newSize.width, (image.size.height / image.size.width) * newSize.width);
+    }
+
+    return newSize;
+}
+
+- (void)saveImage:(UIImage *)image toFilePath:(NSString *)filePath {
+    NSData *pngData = UIImagePNGRepresentation(image);
+
+    [pngData writeToFile:filePath atomically:YES];
+}
+
+- (UIImage *)loadImage:(NSString *)filePath {
+    NSData *imgData = [NSData dataWithContentsOfFile:filePath];
+
+    return [UIImage imageWithData:imgData];
+}
+
+- (NSString *)documentsPathForFileName:(NSString *)name {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsPath = [paths objectAtIndex:0];
+
+    return [documentsPath stringByAppendingPathComponent:name];
 }
 
 @end
